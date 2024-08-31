bc8143e6e2c45fe9a79a50e767b9d0f700daf2d0
zhir4web <zherjalal123@gmail.com>
2024-08-31 10:11:44 -0700

    Delete src directory

--- src/script.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Web developer", "Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true,
-})
-
