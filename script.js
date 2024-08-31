ee55f1c7fa4862b7174888f398627d4a4df57846
zhir4web <zherjalal123@gmail.com>
2024-08-31 10:34:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Web developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
+
