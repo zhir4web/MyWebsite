4807368e9494e7e3c1bb2cb58bc0386595deb15f
Zhir Muslim <zherjalal123@gmail.com>
2024-09-02 08:28:29 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,4 +5,22 @@ var typed = new Typed(".multiple-text", {
     backDelay: 1000,
     loop: true,
 })
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
 
+    menuToggle.addEventListener('click', function() {
+        if (menu.classList.contains('hidden')) {
+            menu.classList.remove('hidden');
+        } else {
+            menu.classList.add('hidden');
+        }
+    });
+});
+   // Dark mode toggle script
+   const toggleButton = document.getElementById('dark-mode-toggle');
+   const body = document.body;
+
+   toggleButton.addEventListener('click', () => {
+     body.classList.toggle('dark-mode');
+   });
